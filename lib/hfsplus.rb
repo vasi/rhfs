ce39236d7336df05e4e0b165503e4f7ea95b9c6a
Dave Vasilevsky <dave@vasilevsky.ca>
2013-03-21 02:46:19 -0400

    HFS+ header

---
+++ lib/hfsplus.rb
@@ -0,0 +1,64 @@
+require 'rubygems'
+require 'bindata'
+
+require_relative 'apm'
+
+# Apple's HFS+ format
+# See hfs_format.h and Apple Tech Note 1150
+class HFSPlus
+	# HFS+ does not use sectors, but bytes
+	HeaderOffset = 1024
+	
+	class ExtentDesc < BERecord
+		uint32	:startBlock
+		uint32	:blockCount
+	end
+	
+	class ForkData < BERecord
+		uint64	:logicalSize
+		uint32	:clumpSize
+		uint32	:totalBlocks
+		array	:extents, :type => :extentDesc, :initial_length => 8
+	end
+	
+	class Header < BERecord
+		string	:signature, :length => 2
+		uint16	:version
+		uint32	:attributes
+		string	:lastMountedVersion, :length => 4
+		uint32	:journalInfoBlock
+		
+		uint32	:createDate
+		uint32	:modifyDate
+		uint32	:backupDate
+		uint32	:checkedDate
+		
+		uint32	:fileCount
+		uint32	:folderCount
+		
+		uint32	:blockSize
+		uint32	:totalBlocks
+		uint32	:freeBlocks
+		
+		uint32	:nextAllocation
+		uint32	:rsrcClumpSize
+		uint32	:dataClumpSize
+		uint32	:nextCatalogID
+		
+		uint32	:writeCount
+		uint64	:encodingsBitmap
+		
+		array	:finderInfo, :type => :uint32, :initial_length => 8
+		
+		forkData	:allocationFile
+		forkData	:extentsFile
+		forkData	:catalogFile
+		forkData	:attributesFile
+		forkData	:startupFile
+	end
+	
+	def initialize(buf)
+		@buf = buf
+		@header = @buf.st_read(Header, HeaderOffset)
+	end
+end
